448cb8eda57594f4fe2e97c908957afd7deb2c11
Mounika Nimmu <mounika.nimmu@gmail.com>
2021-02-17 23:06:10 +0530

    added API
    added API

--- router.js
+++ router.js
@@ -8,5 +8,18 @@ router.get('/', function(req, res) {
         message: 'Welcome to Swapsoul Rest API'
     });
 });
+
+//Import Product Controller
+var productController = require('./productController');
+// Product routes
+router.route('/product')
+    .get(productController.index)
+    .post(productController.add);
+router.route('/product/:productID')
+    .get(productController.view)
+    .patch(productController.update)
+    .put(productController.update)
+    .delete(productController.delete);
+
 //Export API routes
 module.exports = router;
