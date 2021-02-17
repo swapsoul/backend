d45d2badaa9963bfa39a4f10369489caa6cb6bc9
phoenixnitin <nitinkumar6912@gmail.com>
2021-02-18 01:07:57 +0530

    Fix: reading post request data

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 let express = require('express')
 let app = express();
 var port = process.env.PORT || 4000;
+
+//import body parser
+let bodyParser = require('body-parser');
+//configure bodyparser to hande the post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 // Welcome message
 app.get('/', (req, res) => res.send('Welcome to Express'));
 // Launch app to the specified port
@@ -12,15 +20,10 @@ app.listen(port, function() {
 let apiRoutes = require("./router")
 //Use API routes in the App
 app.use('/api', apiRoutes)
-//import body parser
-let bodyParser = require('body-parser');
+
 //import mongoose
 let mongoose = require('mongoose');
-//configure bodyparser to hande the post requests
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
+
 //connect to mongoose
 const dbPath = 'mongodb://swapsoul:swap%21123@cluster0-shard-00-00.bz4pl.mongodb.net:27017,cluster0-shard-00-01.bz4pl.mongodb.net:27017,cluster0-shard-00-02.bz4pl.mongodb.net:27017/test?replicaSet=atlas-ydidtn-shard-0&ssl=true&authSource=admin';
 const options = {useNewUrlParser: true, useUnifiedTopology: true}
