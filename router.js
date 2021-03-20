a461474d013f5f9c0493256121962dfbc54e233f
prabhu yadav p.s <prabhuyadav007.p.y@gmail.com>
2021-03-20 18:10:27 +0530

    Update router.js

--- router.js
+++ router.js
@@ -21,5 +21,29 @@ router.route('/product/:productID')
     .put(productController.update)
     .delete(productController.delete);
 
+// pincode route
+const connection = mongoose.connection;
+router.route("/pincode").post(function (req, res) {
+  const pinCode = req.body.pin;
+
+  connection.db.collection("pinCodeCollection", function (err, pincodes) {
+    pincodes.find({ number: pinCode }).toArray(function (err, data) {
+      if (!data) {
+        console.log("Delivery is available");
+        res.json({
+          status: "success",
+          message: "Delivery is available",
+        });
+      } else {
+        res.json({
+          status: "error",
+          message: err,
+        });
+        console.log("Error is occurreds");
+      }
+    });
+  });
+});
+
 //Export API routes
 module.exports = router;
