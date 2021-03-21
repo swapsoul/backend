33c001908b1bb4e836aadfda45edfc01b8d737ec
phoenixnitin <nitinkumar6912@gmail.com>
2021-03-22 02:33:48 +0530

    update username to usernameOrEmail

--- src/wrappers/auth-wrapper.js
+++ src/wrappers/auth-wrapper.js
@@ -4,17 +4,17 @@ exports.verifyToken = (req, res, next) => {
     if (req.headers['swapsoultoken']) {
         let response = commonService.decryptToken(req.headers['swapsoultoken']);
         console.log(response);
-        if (req.body.username === response.username && commonService.createsha512hash('12345') === response.hash) {
-            console.log('Authorized ' + req.body.username);
+        if (req.body.usernameOrEmail === response.usernameOrEmail && commonService.createsha512hash('12345') === response.hash) {
+            console.log('Authorized ' + req.body.usernameOrEmail);
             next();
         } else {
-            console.log('Unauthorized ' + req.body.username);
+            console.log('Unauthorized ' + req.body.usernameOrEmail);
             res.status(401).json({
                 message: 'Unauthorized'
             });
         }
     } else {
-        console.log('Unauthorized ' + req.body.username);
+        console.log('Unauthorized ' + req.body.usernameOrEmail);
         res.status(401).json({
             message: 'Unauthorized'
         });
